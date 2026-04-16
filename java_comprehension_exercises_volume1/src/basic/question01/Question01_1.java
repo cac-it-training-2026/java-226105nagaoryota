0b552b7ea324af6831e239d5404bf214c44fe525
rnagao <rnagao@C010423416.zipangu.cac.co.jp>
2026-04-16 12:14:36 +0900

    基礎問題完了しました

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
@@ -9,7 +9,18 @@ public class Question01_1 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
-		a
+		//初期化
+		int price = 120;
+		String item = "ノート";
+		//表示
+		System.out.println(price);
+		System.out.println(item);
+		//代入
+		price = 200;
+		item = "鉛筆";
+		//表示
+		System.out.println(price);
+		System.out.println(item);
 
 	}
 
